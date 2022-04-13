d27ce2a1ddef8595a37e8f9aae7d1762452176ba
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2022-04-13 22:45:02 +0200

    More, radical, cleanup

--- emacs/install_prerequisites.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt install libgtk-3-dev libjpeg-dev libpng-dev libtiff-dev libgnutls28-dev libgif-dev libxpm-dev libncurses-dev
--- gnome/settings.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.shell.app-switcher current-workspace-only true
