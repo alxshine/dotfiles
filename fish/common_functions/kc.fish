4808d8b8abffc35898e1765d81574dbc04f546af
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2022-11-09 11:03:54 +0100

    Fix bug in kc function

--- fish/common_functions/kc.fish
+++ fish/common_functions/kc.fish
@@ -1,6 +1,7 @@
 function kc --description 'connect to kak session in current project'
   set name (broot)
-  set socket_file "/run/user/(id -u)/kakoune/$name"
+  set uid (id -u)
+  set socket_file "/run/user/$uid/kakoune/$name"
   if test ! -e $socket_file
         kak -d -s $name &
         sleep 0.1
