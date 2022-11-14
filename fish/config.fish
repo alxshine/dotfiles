a133cdfd9dca4c7bc593d66e15e56658982d4766
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2022-11-14 10:44:20 +0100

    Make kak default editor
    It should be available on all my systems

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -a fish_function_path ~/.config/fish/common_functions
     set -g -a PATH ~/.local/bin ~/.local/opt/julia/bin
+    set -gx EDITOR kak
 end
