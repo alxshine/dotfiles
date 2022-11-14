5dfd6c661479395e44a47bb15f5a94732945279c
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2022-11-14 13:13:08 +0100

    removy jenny from the scrum function

--- fish/common_functions/scrum.fish
+++ fish/common_functions/scrum.fish
@@ -1,3 +1,3 @@
-function scrum --wraps=Rscript\ -e\ \'writeLines\(paste\(c\(\"Rainer\",\ sample\(c\(\"Martin\",\ \"Judith\",\ \"Manuel\",\ \"Alex\",\ \"Max\",\ \"Jenny\",\ \"Paulina\",\ \"Svetlana\",\ \"Nora\",\ \"Leonid\",\ \"Thomas\",\ \"Benedikt\"\)\),\ \"and\ guests\"\),\ collapse=\",\ \"\)\)\' --description alias\ scrum\ Rscript\ -e\ \'writeLines\(paste\(c\(\"Rainer\",\ sample\(c\(\"Martin\",\ \"Judith\",\ \"Manuel\",\ \"Alex\",\ \"Max\",\ \"Jenny\",\ \"Paulina\",\ \"Svetlana\",\ \"Nora\",\ \"Leonid\",\ \"Thomas\",\ \"Benedikt\"\)\),\ \"and\ guests\"\),\ collapse=\",\ \"\)\)\'
-  Rscript -e 'writeLines(paste(c("Rainer", sample(c("Martin", "Judith", "Manuel", "Alex", "Max", "Jenny", "Paulina", "Svetlana", "Nora", "Leonid", "Thomas", "Benedikt")), "and guests"), collapse=", "))' $argv; 
+function scrum
+  Rscript -e 'writeLines(paste(c("Rainer", sample(c("Martin", "Judith", "Manuel", "Alex", "Max", "Paulina", "Svetlana", "Nora", "Leonid", "Thomas", "Benedikt")), "and guests"), collapse=", "))' $argv; 
 end
