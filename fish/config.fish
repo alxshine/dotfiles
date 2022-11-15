e7ebd108352056d1d24dfceab9ed3dc5f1e41281
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2022-11-15 14:53:57 +0100

    Move common stuff to conf.d

--- fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -a fish_function_path ~/.config/fish/common_functions
-    set -g -a PATH ~/.local/bin ~/.local/opt/julia/bin
-    set -gx EDITOR kak
-end
