0458a29f29ad5cf95861aa25380d529957047c45
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2023-01-26 12:44:01 +0100

    make kc function more succinct

--- fish/conf.d/kc.fish
+++ fish/conf.d/kc.fish
@@ -1,11 +1,12 @@
 function kc --description 'connect to kak session in current project'
   set name (broot)
-  set uid (id -u)
-  set socket_file "/run/user/$uid/kakoune/$name"
-  if test ! -e $socket_file
-        kak -d -s $name &
-        sleep 0.1
-        disown
-  end
-  kak -c $name $argv
+  kak -c $name $argv || kak -s $name $argv
+  # set uid (id -u)
+  # set socket_file "/run/user/$uid/kakoune/$name"
+  # if test ! -e $socket_file
+  #       kak -d -s $name &
+  #       sleep 0.1
+  #       disown
+  # end
+  # kak -c $name $argv
 end
