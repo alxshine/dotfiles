910f2e6e4692aa81b262e400c38f92cbb16c5854
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2023-01-31 10:35:09 +0100

    Make launch script agnostic of screen setup

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/fish
 
 killall -q polybar
-polybar &
+for line in (polybar -m)
+  echo $line | cut -f 1 -d ":" | read monitor
+  if echo $line | rg primary > /dev/null
+    set bar primary
+  else
+    set bar secondary
+  end
+
+  MONITOR=$monitor polybar $bar 2>/dev/null &
+end
+
