06922b351e4929b4dfcc2ab7282641e444fa90e3
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2023-02-02 12:57:12 +0100

    remove max from scrum list

--- fish/conf.d/scrum.fish
+++ fish/conf.d/scrum.fish
@@ -1,3 +1,3 @@
 function scrum
-  Rscript -e 'writeLines(paste(c("Rainer", sample(c("Martin", "Judith", "Manuel", "Alex", "Max", "Paulina", "Svetlana", "Nora", "Leonid", "Thomas", "Benedikt")), "and guests"), collapse=", "))' $argv; 
+  Rscript -e 'writeLines(paste(c("Rainer", sample(c("Martin", "Judith", "Manuel", "Alex", "Paulina", "Svetlana", "Nora", "Leonid", "Thomas", "Benedikt")), "and guests"), collapse=", "))' $argv; 
 end
