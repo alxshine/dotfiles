cd5c6922d41d955367cd37dd12101712c63af54c
Alex Schlögl <alexander.schloegl@uibk.ac.at>
2023-06-29 14:24:08 +0200

    add command to add my nicety scripts in python

---
+++ fish/conf.d/pyniceness.fish
@@ -0,0 +1,4 @@
+function pyniceness --wraps='pip install ipykernel black pdbpp ipython' --description 'alias pyniceness pip install ipykernel black pdbpp ipython'
+  pip install ipykernel black pdbpp ipython $argv
+        
+end
