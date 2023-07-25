1fc052178aa4f43dc858f0db126496784bb45d15
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2023-07-25 12:51:55 +0200

    fetch before rebasing

--- fish/conf.d/grb.fish
+++ fish/conf.d/grb.fish
@@ -1,4 +1,4 @@
 function grb --wraps='git rebase (git rev-parse --abbrev-ref HEAD)' --wraps='git rebase (git rev-parse --abbrev-ref origin/HEAD)' --description 'alias grb git rebase (git rev-parse --abbrev-ref origin/HEAD)'
-  git rebase (git rev-parse --abbrev-ref origin/HEAD) $argv
+  git fetch && git rebase (git rev-parse --abbrev-ref origin/HEAD) $argv
         
 end
