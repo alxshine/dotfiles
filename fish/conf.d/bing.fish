9842f72803da2cd998478e5791b966f1659b05fe
Alex Schlögl <alexander.schloegl@uibk.ac.at>
2023-08-09 18:22:09 +0200

    update fish commands

---
+++ fish/conf.d/bing.fish
@@ -0,0 +1,4 @@
+function bing --wraps='notify-send "done :)"' --description 'alias mping notify-send "done :)"'
+  notify-send -t 10000 "This is your requested BING :)" $argv
+        
+end
--- fish/conf.d/bluetooth_toggle.fish
+++
@@ -1,7 +0,0 @@
-function bluetooth_toggle
-if bluetoothctl show | rg "PowerState: on"
-bluetoothctl power off
-else
-bluetoothctl power on
-end
-end
