88b894a24ed52ad7562a24654e4b07b6bb647322
Alex Schlögl <alexander.schloegl@uibk.ac.at>
2023-09-07 20:33:09 +0200

    add 1password to unislice

--- fish/conf.d/vpn.fish
+++ fish/conf.d/vpn.fish
@@ -3,5 +3,5 @@ function uniconnect
     sudo openconnect vpn.uibk.ac.at -u c7031199
 end
 function unislice 
-    sudo openconnect vpn.uibk.ac.at -u c7031199 -s 'vpn-slice 138.232.0.0/16'
+        op item get LFU --fields label=password | sudo openconnect vpn.uibk.ac.at --passwd-on-stdin -u c7031199 -s 'vpn-slice 138.232.0.0/16'
 end
