a2ecae5e7bb6eb2520a994973107aedb09007163
Alex Schlögl <alexander.schloegl@uibk.ac.at>
2023-09-14 15:02:44 +0200

    add autoslice command

--- fish/conf.d/vpn.fish
+++ fish/conf.d/vpn.fish
@@ -3,5 +3,11 @@ function uniconnect
     sudo openconnect vpn.uibk.ac.at -u c7031199
 end
 function unislice 
-        op item get LFU --fields label=password | sudo openconnect vpn.uibk.ac.at --passwd-on-stdin -u c7031199 -s 'vpn-slice 138.232.0.0/16'
+        sudo openconnect vpn.uibk.ac.at -u c7031199 -s 'vpn-slice 138.232.0.0/16'
 end
+
+function autoslice
+        firefox https://fwauth-tech.uibk.ac.at
+        op item get LFU --fields label=password | sudo openconnect vpn.uibk.ac.at --passwd-on-stdin -u c7031199
+end
+
