755bfe10a3d2d7d9724bf10ea78993b14a8487d4
Alex Schlögl <alxshine@proton.me>
2023-11-13 15:26:40 +0100

    update configs

---
+++ fish/conf.d/setup_git.fish
@@ -0,0 +1,9 @@
+function setup_git_rs --description 'set git details to rootsys for project'
+    git config user.name "Alex Schlögl"
+    git config user.email "a.schloegl@rootsys.at"
+end
+    
+function setup_git_priv --description 'set git details to private for project'
+    git config user.name "Alex Schlögl"
+    git config user.email "alxshine@proton.me"
+end
--- git/setup.sh
+++ git/setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-git config --global user.name "Alex Schlögl"
-git config --global user.email "alxshine@proton.me"
-
 git config --global core.editor helix
 git config --global init.defaultBranch main
